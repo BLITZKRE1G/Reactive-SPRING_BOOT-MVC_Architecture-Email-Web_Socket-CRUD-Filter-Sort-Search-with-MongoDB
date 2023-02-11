6fafd625ac9a03e9efd0122909e27cf88dc73091
Abhinab <abhinab_gupta@thbs.com>
2023-02-11 17:24:50 +0530

    Done

--- crud/src/main/java/personal/project/crud/configuration/WebSocketConfig.java
+++ crud/src/main/java/personal/project/crud/configuration/WebSocketConfig.java
@@ -1,8 +1,9 @@
 package personal.project.crud.configuration;
 
+import org.springframework.boot.autoconfigure.graphql.reactive.GraphQlWebFluxAutoConfiguration.WebSocketConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class WebSocketConfig {
+public class WebSocketConfig extends WebSocketConfiguration {
 
 }
