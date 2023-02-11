0dad9be11a2b61673aac8ff194b74c37a386e689
Abhinab <abhinab_gupta@thbs.com>
2023-02-11 18:12:33 +0530

    Configuring

--- crud/src/main/java/personal/project/crud/configuration/WebSocketConfig.java
+++ crud/src/main/java/personal/project/crud/configuration/WebSocketConfig.java
@@ -1,8 +1,10 @@
 package personal.project.crud.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @Configuration
+@EnableWebFlux
 public class WebSocketConfig {
 
 }
